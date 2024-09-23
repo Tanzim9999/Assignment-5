b46a0929ebe5fcaecfc1335abbf51162956447a0
Tanzim9999 <tanzimboy1@gmail.com>
2024-09-23 15:42:19 +0600

    added function of clicking history and donation section

---
+++ indexjava.js
@@ -0,0 +1,2 @@
+document.getElementById('donateButton').addEventListener('click',function(){clickSection('donationSection')});
+document.getElementById('historyButton').addEventListener('click',function(){clickSection('historySection')});
