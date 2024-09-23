5df8a76f743157216a5eca33467e914a44b213d6
Tanzim9999 <tanzimboy1@gmail.com>
2024-09-23 23:29:01 +0600

    added more functions

--- indexjava.js
+++ indexjava.js
@@ -5,12 +5,16 @@ document.getElementById('historyButton').addEventListener('click',function(){cli
 document.getElementById('historyButton').addEventListener('click',function(){buttonColor('historyButton')});
 document.getElementById('donateButton').addEventListener('click',function(){buttonColor('donateButton')});
 // Function to send alert for invalid inputs
+    
 document.getElementById('donateNow').addEventListener('click',function(){
-    const donateAmount = document.getElementById('inputField1').value
-    const balance = document.getElementById('balance').innerText
-    console.log(balance)
-    const sectionBalance = document.getElementById('sectionBalance').innerText
+    const donateAmount = parseFloat(document.getElementById('inputField1').value)
+    const balance = parseFloat(document.getElementById('balance').innerText)
+    const sectionBalance = parseFloat(document.getElementById('sectionBalance').innerText)
+    if (isNaN(donateAmount) || donateAmount <= 0 || donateAmount > balance ){
+        alert ("Sorry Invalid Format or Donation Amount is more than your Balance");}
+    else{ 
     const newBalance = balance - donateAmount
     const newSectionBalance = sectionBalance + donateAmount
-    console.log(newBalance, newSectionBalance);
-})
+    document.getElementById('balance').innerText = newBalance;
+    document.getElementById('sectionBalance').innerText = newSectionBalance;
+}});
