6b920bd7305d0618e2d1607415b055507d0948ff
Tanzim9999 <tanzimboy1@gmail.com>
2024-09-23 16:41:00 +0600

    added function (still in dev)

--- indexjava.js
+++ indexjava.js
@@ -4,3 +4,13 @@ document.getElementById('historyButton').addEventListener('click',function(){cli
 // button color changing functions
 document.getElementById('historyButton').addEventListener('click',function(){buttonColor('historyButton')});
 document.getElementById('donateButton').addEventListener('click',function(){buttonColor('donateButton')});
+// Function to send alert for invalid inputs
+document.getElementById('donateNow').addEventListener('click',function(){
+    const donateAmount = document.getElementById('inputField1').value
+    const balance = document.getElementById('balance').innerText
+    console.log(balance)
+    const sectionBalance = document.getElementById('sectionBalance').innerText
+    const newBalance = balance - donateAmount
+    const newSectionBalance = sectionBalance + donateAmount
+    console.log(newBalance, newSectionBalance);
+})
